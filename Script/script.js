14a447e5f021ba244f20dac4ae345395c3d37175
Samir <samirspacetech@gmail.com>
2022-02-23 15:51:11 +0100

    [UPDATE]
    Fin du projet. Réglage de quelques problèmes de position sur les messages d'erreur et de validation, formulaire fonctionnel.

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,64 @@
+let form = document.querySelector('form')
+let inputNom = document.querySelector('#nom')
+let inputMail = document.querySelector('#mail')
+let regleNom = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/;
+let regleMail =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+let resultatNom = regleNom.test(inputNom.value)
+let resultatMail = regleMail.test(inputMail.value)
+let button = document.getElementById("subbtn")
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let resultatNom = regleNom.test(inputNom.value)
+    let resultatMail = regleMail.test(inputMail.value)
+
+    if ((resultatNom == false) && (resultatMail == true)) {
+        document.getElementById('erreur').style.visibility= 'visible';
+        document.getElementById('erreur').innerHTML='Veuillez renseigner un nom valide.'
+        document.getElementById('erreur').style.background= 'red'
+
+    } else if((resultatNom == true) && (resultatMail == false)) {
+        document.getElementById('erreur').style.visibility= 'visible';
+        document.getElementById('erreur').innerHTML='Veuillez renseigner une adresse mail valide.'
+        document.getElementById('erreur').style.background= 'red'
+    } else if ((resultatNom == false) && (resultatMail == false)) {
+        document.getElementById('erreur').style.visibility= 'visible';
+        document.getElementById('erreur').innerHTML='Veuillez renseigner des informations valide.'
+        document.getElementById('erreur').style.background= 'red'
+    } else {
+        document.getElementById('erreur').style.visibility= 'hidden';
+    }
+
+
+    let formData = new FormData(form)
+    let url = './PHP/formulaire.php';
+    fetch(url,{
+        method: 'POST',
+        body: formData
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+
+        document.querySelector('.send').innerHTML=data.validation
+        return form.reset()
+        
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
 let facebookcircle = document.getElementById('facebook');
 let iconF = document.getElementById('hover-color-facebook');
 
@@ -7,31 +68,40 @@ let iconT = document.getElementById('hover-color-twitter');
 let instagramcircle = document.getElementById('instagram');
 let iconI = document.getElementById('hover-color-instagram');
 
-iconF.addEventListener('mouseenter', e =>{
-    facebookcircle.style.background = '#fff';
 
-})
+if (window.matchMedia("(min-width: 900px)").matches) {
 
-iconF.addEventListener('mouseleave', e =>{
-    facebookcircle.style.background = 'none';
-})
+    iconF.addEventListener('mouseenter', e =>{
 
-iconT.addEventListener('mouseenter', e =>{
-    twittercircle.style.background = '#fff';
+        facebookcircle.style.background = '#fff';
+        
+        })
+        
+        iconF.addEventListener('mouseleave', e =>{
+        facebookcircle.style.background = 'none';
+        })
+        
+        iconT.addEventListener('mouseenter', e =>{
+        twittercircle.style.background = '#fff';
+        
+        })
+        
+        iconT.addEventListener('mouseleave', e =>{
+        twittercircle.style.background = 'none';
+        })
+        
+        iconI.addEventListener('mouseenter', e =>{
+        instagramcircle.style.background = '#fff';
+        
+        })
+        
+        iconI.addEventListener('mouseleave', e =>{
+        instagramcircle.style.background = 'none';
+        })
 
-})
+    
 
-iconT.addEventListener('mouseleave', e =>{
-    twittercircle.style.background = 'none';
-})
+}else {
 
-iconI.addEventListener('mouseenter', e =>{
-    instagramcircle.style.background = '#fff';
-
-})
-
-iconI.addEventListener('mouseleave', e =>{
-    instagramcircle.style.background = 'none';
-})
-
+}
 
